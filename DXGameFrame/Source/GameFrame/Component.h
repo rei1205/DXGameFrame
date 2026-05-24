56a1bf76291929d9bbf207b10010a366325134f1
rei1205 <rei1205.creator@gmail.com>
2026-05-24 17:26:06 +0900

    GameObject/Componentの宣言追加

---
+++ DXGameFrame/Source/GameFrame/Component.h
@@ -0,0 +1,97 @@
+// Component.h
+#pragma once
+#include "Object.h"
+
+
+class GameObject;
+class Transform;
+
+
+/**
+ * @brief コンポーネントの基底クラス
+ */
+class Component : public Object
+{
+public:
+	Component();
+	virtual ~Component() = default;
+
+	/**
+	 * @brief 親ゲームオブジェクトを取得する
+	 * @return 親ゲームオブジェクトへのポインタ
+	 */
+	GameObject* GetGameObject()
+	{
+		return m_pGameObject;
+	}
+
+	/**
+	 * @brief Transformコンポーネントを取得する
+	 * @return Transformコンポーネントへのポインタ
+	 */
+	Transform* GetTransform()
+	{
+		return m_pTransform;
+	}
+
+	/**
+	 * @brief 親子関係を考慮した有効状態を取得する
+	 * @return ヒエラルキー上の有効状態
+	 */
+	bool IsActiveHierarchy();
+
+	/**
+	 * @brief このコンポーネントの有効状態を取得する
+	 * @return このコンポーネントの有効状態
+	 */
+	bool IsEnabled() const
+	{
+		return m_isEnabled;
+	}
+
+	/**
+	 * @brief このコンポーネントの有効状態を設定する
+	 * @param isEnabled 設定する有効状態
+	 */
+	void SetEnabled(bool isEnabled)
+	{
+		m_isEnabled = isEnabled;
+	}
+
+	/**
+	 * @brief 開始処理の呼び出し状態を取得する
+	 * @return 開始処理呼び出し済みフラグ
+	 */
+	bool IsStarted()
+	{
+		return m_isStarted;
+	}
+
+	/**
+	 * @brief 開始処理呼び出し済みフラグをオンにする
+	 */
+	void MarkStartedFlag()
+	{
+		m_isStarted = true;
+	}
+
+private:
+	/// 親ゲームオブジェクトへのポインタ
+	GameObject* m_pGameObject;
+
+	/// Transformコンポーネントへのポインタ
+	Transform* m_pTransform;
+
+	/// このコンポーネントの有効状態
+	bool m_isEnabled;
+
+	/// 開始処理呼び出し済みフラグ
+	bool m_isStarted;
+
+	/**
+	 * @brief コンポーネントの初期化を行う
+	 * @param pGameObject 親ゲームオブジェクトへのポインタ
+	 * @param pTransform Transformコンポーネントへのポインタ
+	 */
+	void Init(GameObject* pGameObject, Transform* pTransform);
+};
---
+++ DXGameFrame/Source/GameFrame/GameObject.h
@@ -0,0 +1,82 @@
+// GameObject.h
+#pragma once
+#include "Component.h"
+
+
+class Scene;
+
+
+/**
+ * @brief コンポーネントの追加・取得・削除などを行う
+ */
+class GameObject : public Object
+{
+	friend class Scene;
+
+public:
+	GameObject() = default;
+	~GameObject() = default;
+
+	/**
+	 * @brief コンポーネントを取得する
+	 * @return コンポーネントへのポインタ
+	 */
+	template <typename T>
+	T* GetComponent();
+
+	/**
+	 * @brief コンポーネントを追加する
+	 * @return 追加したコンポーネントへのポインタ
+	 */
+	template <typename T>
+	T* AddComponent();
+
+	/**
+	 * @brief コンポーネントを削除する
+	 */
+	template <typename T>
+	void RemoveComponent();
+
+	/**
+	 * @brief Transformコンポーネントを取得する
+	 * @return Transformコンポーネントへのポインタ
+	 */
+	Transform* GetTransform()
+	{
+		return m_pTransform;
+	}
+
+	/**
+	 * @brief 親子関係を考慮した有効状態を取得する
+	 * @return ヒエラルキー上の有効状態
+	 */
+	bool IsActiveHierarchy() const;
+
+	/**
+	 * @brief このゲームオブジェクト自身の有効状態を取得する
+	 * @return このゲームオブジェクトの有効状態
+	 */
+	bool IsActiveSelf() const
+	{
+		return m_isActive;
+	}
+
+	/**
+	 * @brief このゲームオブジェクトの有効状態を設定する
+	 * @param isActive 設定する有効状態
+	 */
+	void SetActive(bool isActive)
+	{
+		m_isActive = isActive;
+	}
+
+private:
+	/// このゲームオブジェクトが持つコンポーネントのリスト
+	std::vector<Component*> m_components;
+	
+	/// Transformコンポーネントへのポインタ
+	Transform* m_pTransform;
+
+	/// このゲームオブジェクトの有効状態
+	bool m_isActive;
+};
