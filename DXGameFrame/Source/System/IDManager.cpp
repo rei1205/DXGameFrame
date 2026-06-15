c15e9946ed2630646e38d38a63f92936784be0e6
rei1205 <rei1205.creator@gmail.com>
2026-06-15 17:26:46 +0900

    IDManager改修

--- DXGameFrame/Source/System/IDManager.cpp
+++ DXGameFrame/Source/System/IDManager.cpp
@@ -1,38 +1,65 @@
 // IDManager.cpp
 #include "IDManager.h"
 
-IDManager::IDManager() :
-	m_nextID(0)
+IDManager::IDManager(bool useZero):
+	m_useZero(useZero)
 {
+	m_nextID = useZero ? 0 : 1;
 }
 
 uint32_t IDManager::GenerateID()
 {
 	uint32_t id;
 
-	if (m_freeIDs.empty())
+	if (!m_freeIDs.empty())
 	{
-		// IDを新規生成
-		id = m_nextID;
-		m_nextID++;
+		// 生成済みIDを再利用
+		id = m_freeIDs.back();
+		m_freeIDs.pop_back();
 		return id;
 	}
 
-	// 生成済みIDを再利用
-	id = m_freeIDs.back();
-	m_freeIDs.pop_back();
+	// 新規生成IDを取得
+	while (m_generatedIDs.contains(m_nextID))
+	{
+		m_nextID++;
+	}
+
+	// IDを新規生成
+	id = m_nextID;
+	m_generatedIDs.insert(id);
+	m_nextID++;
 	return id;
+}
+
+bool IDManager::RegisterID(uint32_t id)
+{
+	// 解放済みIDから検索
+	auto it = std::find(m_freeIDs.begin(), m_freeIDs.end(), id);
+	if (it != m_freeIDs.end())
+	{
+		m_freeIDs.erase(it);
+		return true;
+	}
+
+	// 使用済みIDか調べる
+	if (m_generatedIDs.contains(id))
+		return false;
+
+	// 新規IDとして生成
+	m_generatedIDs.insert(id);
+	return true;
 }
 
 void IDManager::ReleaseID(uint32_t id)
 {
-	// 値チェック
-	if (id < 0 || id >= m_nextID)
+	// 生成済みIDかチェック
+	if (!m_generatedIDs.contains(id))
 		return;
 
 	// 既に解放済みかチェック
 	auto it = std::find(m_freeIDs.begin(), m_freeIDs.end(), id);
-	if (it == m_freeIDs.end())
+	if (it != m_freeIDs.end())
 		return;
 
 	// IDを解放
@@ -42,5 +69,6 @@ void IDManager::ReleaseID(uint32_t id)
 void IDManager::Clear()
 {
 	m_freeIDs.clear();
-	m_nextID = 0;
+	m_generatedIDs.clear();
+	m_nextID = m_useZero ? 0 : 1;
 }
--- DXGameFrame/Source/System/IDManager.h
+++ DXGameFrame/Source/System/IDManager.h
@@ -1,6 +1,7 @@
 // IDManager.h
 #pragma once
-#include <vector>
+#include <unordered_set>
+
 
 /**
  * @brief 一意なIDの生成・管理を行う
@@ -8,7 +9,7 @@
 class IDManager
 {
 public:
-	IDManager();
+	IDManager(bool useZero = true);
 	~IDManager() = default;
 
 	/**
@@ -16,6 +17,13 @@ public:
 	 * @return 生成したID
 	 */
 	uint32_t GenerateID();
+
+	/**
+	 * @brief 任意のIDを登録する
+	 * @param id 登録するID
+	 * @return 既にIDが使用されていた場合falseを返す
+	 */
+	bool RegisterID(uint32_t id);
 
 	/**
 	 * @brief IDを解放する
@@ -32,6 +40,12 @@ private:
 	/// 次に新規生成するID
 	uint32_t m_nextID;
 
+	/// 生成済みID
+	std::unordered_set<uint32_t> m_generatedIDs;
+
 	/// 生成済みで現在使用されていないID
 	std::vector<uint32_t> m_freeIDs;
+
+	/// IDの0番を使用するかの設定
+	bool m_useZero = false;
 };
